52c33a92255d954ceda66325809805e8ccb55a81
Francinildo Melo Rodrigues <francis584@gmail.com>
2016-10-19 20:08:07 -0300

    Criado domain e controller Usuario

---
+++ grails-app/controllers/quiz/UsuarioController.groovy
@@ -0,0 +1,7 @@
+package quiz
+
+class UsuarioController {
+
+    static scaffold = Usuario
+
+}
--- grails-app/domain/quiz/Usuario.groovy
+++ grails-app/domain/quiz/Usuario.groovy
@@ -2,6 +2,16 @@ package quiz
 
 class Usuario {
 
+  String nome
+  String matricula
+  String senha
+  String email
+
     static constraints = {
+      nome nullable:false, blank:false, maxSize: 128
+      matricula nullable:false, blank:false, maxSize: 15, unique:true
+      senha nullable:false, blank:false, maxSize: 15
+      email nullable:false, blank:false, maxSize: 45, email:true
+
     }
 }
